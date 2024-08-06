46bd15f397926acf7bb95095cd3f5eeb919bf3ff
karaivan2000 <133887129+karaivan2000@users.noreply.github.com>
2024-08-07 02:10:02 +0300

    Add abstract requester

--- client/src/api/requester.js
+++ client/src/api/requester.js
@@ -1,16 +1,29 @@
-export default async function requester(method, url, data) {
+async function requester(method, url, data) {
     const options = {};
     
     if (method !== `GET`) {
         options.method = method;
     }
 
-    if (data) 
+    if (data) {
         options.headers = {
             "Content-Type": "application/json",
         };
 
         options.body = JSON.stringify(data);
-}
 
-const response = awai fetch(url, options);
+    }
+
+    const response = await fetch(url, options);
+    const result = response.json();  
+
+    return result;
+
+};
+
+export const get = requester.bind(null, `GET`);
+export const post = requester.bind(null, `POST`);
+export const put = requester.bind(null, `PUT`);
+export const del = requester.bind(null, `DELETE`);
+
+
---
+++ client/src/components/recipeList/RecipeList.jsx
@@ -0,0 +1,238 @@
+
+
+export default function RecipeList() {
+
+    return (
+        <div className="cwrap">
+            <header className="s-title">
+                <h2 className="ribbon bright">Latest recipes</h2>
+            </header>
+            {/*entries*/}
+            <div className="entries row">
+                {/*item*/}
+                <div className="entry one-third">
+                    <figure>
+                        <img src="images/img.jpg" alt="" />
+                        <figcaption>
+                            <a href="recipe.html">
+                                <i className="icon icon-themeenergy_eye2" />{" "}
+                                <span>View recipe</span>
+                            </a>
+                        </figcaption>
+                    </figure>
+                    <div className="container">
+                        <h2>
+                            <a href="recipe.html">
+                                Thai fried rice with fruit and vegetables
+                            </a>
+                        </h2>
+                        <div className="actions">
+                            <div>
+                                <div className="difficulty">
+                                    <i className="ico i-medium" />
+                                    <a href="#">medium</a>
+                                </div>
+                                <div className="likes">
+                                    <i className="fa fa-heart" />
+                                    <a href="#">10</a>
+                                </div>
+                                <div className="comments">
+                                    <i className="fa fa-comment" />
+                                    <a href="recipe.html#comments">27</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/*item*/}
+                {/*item*/}
+                <div className="entry one-third">
+                    <figure>
+                        <img src="images/img.jpg" alt="" />
+                        <figcaption>
+                            <a href="recipe.html">
+                                <i className="icon icon-themeenergy_eye2" />{" "}
+                                <span>View recipe</span>
+                            </a>
+                        </figcaption>
+                    </figure>
+                    <div className="container">
+                        <h2>
+                            <a href="recipe.html">
+                                Spicy Morroccan prawns with cherry tomatoes
+                            </a>
+                        </h2>
+                        <div className="actions">
+                            <div>
+                                <div className="difficulty">
+                                    <i className="ico i-hard" />
+                                    <a href="#">hard</a>
+                                </div>
+                                <div className="likes">
+                                    <i className="fa fa-heart" />
+                                    <a href="#">10</a>
+                                </div>
+                                <div className="comments">
+                                    <i className="fa fa-comment" />
+                                    <a href="recipe.html#comments">27</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/*item*/}
+                {/*item*/}
+                <div className="entry one-third">
+                    <figure>
+                        <img src="images/img.jpg" alt="" />
+                        <figcaption>
+                            <a href="recipe.html">
+                                <i className="icon icon-themeenergy_eye2" />{" "}
+                                <span>View recipe</span>
+                            </a>
+                        </figcaption>
+                    </figure>
+                    <div className="container">
+                        <h2>
+                            <a href="recipe.html">Super easy blueberry cheesecake</a>
+                        </h2>
+                        <div className="actions">
+                            <div>
+                                <div className="difficulty">
+                                    <i className="ico i-easy" />
+                                    <a href="#">easy</a>
+                                </div>
+                                <div className="likes">
+                                    <i className="fa fa-heart" />
+                                    <a href="#">10</a>
+                                </div>
+                                <div className="comments">
+                                    <i className="fa fa-comment" />
+                                    <a href="recipe.html#comments">27</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/*item*/}
+                {/*item*/}
+                <div className="entry one-third">
+                    <figure>
+                        <img src="images/img.jpg" alt="" />
+                        <figcaption>
+                            <a href="recipe.html">
+                                <i className="icon icon-themeenergy_eye2" />{" "}
+                                <span>View recipe</span>
+                            </a>
+                        </figcaption>
+                    </figure>
+                    <div className="container">
+                        <h2>
+                            <a href="recipe.html">
+                                Refreshing banana split with a twist for adults
+                            </a>
+                        </h2>
+                        <div className="actions">
+                            <div>
+                                <div className="difficulty">
+                                    <i className="ico i-hard" />
+                                    <a href="#">hard</a>
+                                </div>
+                                <div className="likes">
+                                    <i className="fa fa-heart" />
+                                    <a href="#">10</a>
+                                </div>
+                                <div className="comments">
+                                    <i className="fa fa-comment" />
+                                    <a href="recipe.html#comments">27</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/*item*/}
+                {/*item*/}
+                <div className="entry one-third">
+                    <figure>
+                        <img src="images/img.jpg" alt="" />
+                        <figcaption>
+                            <a href="recipe.html">
+                                <i className="icon icon-themeenergy_eye2" />{" "}
+                                <span>View recipe</span>
+                            </a>
+                        </figcaption>
+                    </figure>
+                    <div className="container">
+                        <h2>
+                            <a href="recipe.html">
+                                Sushi mania: this is the best sushi you have ever tasted
+                            </a>
+                        </h2>
+                        <div className="actions">
+                            <div>
+                                <div className="difficulty">
+                                    <i className="ico i-easy" />
+                                    <a href="#">easy</a>
+                                </div>
+                                <div className="likes">
+                                    <i className="fa fa-heart" />
+                                    <a href="#">10</a>
+                                </div>
+                                <div className="comments">
+                                    <i className="fa fa-comment" />
+                                    <a href="recipe.html#comments">27</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/*item*/}
+                {/*item*/}
+                <div className="entry one-third">
+                    <figure>
+                        <img src="images/img.jpg" alt="" />
+                        <figcaption>
+                            <a href="recipe.html">
+                                <i className="icon icon-themeenergy_eye2" />{" "}
+                                <span>View recipe</span>
+                            </a>
+                        </figcaption>
+                    </figure>
+                    <div className="container">
+                        <h2>
+                            <a href="recipe.html">
+                                Princess puffs - an old classic at its best
+                            </a>
+                        </h2>
+                        <div className="actions">
+                            <div>
+                                <div className="difficulty">
+                                    <i className="ico i-hard" />
+                                    <a href="#">hard</a>
+                                </div>
+                                <div className="likes">
+                                    <i className="fa fa-heart" />
+                                    <a href="#">10</a>
+                                </div>
+                                <div className="comments">
+                                    <i className="fa fa-comment" />
+                                    <a href="recipe.html#comments">27</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/*item*/}
+                <div className="quicklinks">
+                    <a href="#" className="button">
+                        More recipes
+                    </a>
+                    <a href="javascript:void(0)" className="button scroll-to-top">
+                        Back to top
+                    </a>
+                </div>
+            </div>
+            {/*//entries*/}
+        </div>
+    );
+}
