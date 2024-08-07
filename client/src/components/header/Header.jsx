1af32bca0099626f6f2c8908318aae5965aa7a09
karaivan2000 <133887129+karaivan2000@users.noreply.github.com>
2024-08-07 04:51:25 +0300

    Add header page

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -23,84 +23,16 @@ export default function Header() {
                         </li>
                         <li>
                             <Link to="/login" title="Login page">
-                                Login
+                                <span>Login</span>
                             </Link>
                         </li>
 
                         <li>
-                            <Link to="#" title="Pages">
-                                <span>Pages</span>
-                            </Link>
-                            <ul>
-                                <li>
-                                    <Link to="left_sidebar.html" title="Left sidebar page">
-                                        Left sidebar page
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="right_sidebar.html" title="Right sidebar page">
-                                        Right sidebar page
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="two_sidebars.html" title="Both sidebars page">
-                                        Both sidebars page
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="full_width.html" title="Full width page">
-                                        Full width page
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="login.html" title="Login page">
-                                        Login page
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="register.html" title="Register page">
-                                        Register page
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="error404.html" title="Error page">
-                                        Error page
-                                    </Link>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <Link to="#" title="Features">
-                                <span>Features</span>
-                            </Link>
-                            <ul>
-                                <li>
-                                    <Link to="icons.html" title="Icons">
-                                        Icons
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="grid.html" title="Grid">
-                                        Grid
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="shortcodes.html" title="Shortcodes">
-                                        Shortcodes
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="pricing.html" title="Pricing tables">
-                                        Pricing tables
-                                    </Link>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
                             <Link to="/register" title="Register page">
-                                Register 
+                                <span>Register</span>
                             </Link>
                         </li>
+
                         <li>
                             <Link to="/logout">
                                 <span>Logout</span>
@@ -116,12 +48,7 @@ export default function Header() {
                                 <span>Search for recipes</span>
                             </Link>
                         </li>
-                        <li className="medium">
-                            <Link to="my_profile.html" title="My account">
-                                <i className="icon icon-themeenergy_chef-hat" />{" "}
-                                <span>My account</span>
-                            </Link>
-                        </li>
+
                         <li className="dark">
                             <Link to="/recipes/submit" title="Submit a recipe">
                                 <i className="icon icon-themeenergy_fork-spoon" />{" "}
