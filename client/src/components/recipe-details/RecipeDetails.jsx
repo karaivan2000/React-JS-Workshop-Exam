5a220ab97fd1fcf1b05e986182fb26117cd5f917
karaivan2000 <133887129+karaivan2000@users.noreply.github.com>
2024-08-08 15:10:22 +0300

    .

--- client/src/components/recipe-details/RecipeDetails.jsx
+++ client/src/components/recipe-details/RecipeDetails.jsx
@@ -1,1 +1,36 @@
-// To Do details
+<div className="featured two-third">
+<header className="s-title">
+    <h2 className="ribbon">Recipe of the Day</h2>
+</header>
+<article className="entry">
+    <figure>
+        <img src="images/img.jpg" alt="" />
+        <figcaption>
+            <a href="recipe.html">
+                <i className="icon icon-themeenergy_eye2" />{" "}
+                <span>View recipe</span>
+            </a>
+        </figcaption>
+    </figure>
+    <div className="container">
+        <h2>
+            <a href="recipe.html">Honey Cake</a>
+        </h2>
+        <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+            sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+            magna aliquam erat volutpat.{" "}
+        </p>
+        <div className="actions">
+            <div>
+                <a href="#" className="button">
+                    See the full recipe
+                </a>
+                <div className="more">
+                    <a href="recipes2.html">See past recipes of the day</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</article>
+</div>
